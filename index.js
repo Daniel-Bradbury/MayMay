639562e1092a36007ec413e0842d6adb78b38604
Daniel Bradbury <48618519+Daniel-Bradbury@users.noreply.github.com>
2019-04-27 21:50:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ defmeme("nani","https://i.ytimg.com/vi/vxKBHX9Datw/maxresdefault.jpg")
 defmeme("jojolick","https://cdn.discordapp.com/attachments/562372678690930689/571779240693858306/lick-S1QzRJp7z.gif")
 defmeme("jojopunch","https://saechaocirculation.files.wordpress.com/2016/12/star-platinum-vs-crazy-diamond.gif")
 defmeme("nohead","https://cdn.discordapp.com/attachments/562372678690930689/571780405938618378/a2b7468.jpg")
+defmeme("animeheadbang","https://media1.tenor.com/images/e3ee98dbe8f779bd28a7ec3a964bb0c2/tenor.gif")
+defmeme("kden","https://i.kym-cdn.com/entries/icons/mobile/000/016/574/kden.jpg")
 
 });
 
