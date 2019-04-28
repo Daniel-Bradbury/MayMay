7ed381806ffbef466449ab81ec63fb80062bcc14
Daniel Bradbury <48618519+Daniel-Bradbury@users.noreply.github.com>
2019-04-28 11:40:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ defmeme("blink","https://media3.giphy.com/media/3ELtfmA4Apkju/giphy.gif")
 defmeme("fbiwantslocation","https://i.imgur.com/8uZgKCe.jpg")
 defmeme("bekfast","https://s3.amazonaws.com/s3.mp-cdn.net/75/4a/43f0d559653ca0cedbc6629b0f36-bekfast.jpg")
 defmeme("mlgbekfast","https://i.imgflip.com/1qy6m7.jpg")
+defmeme("politecatderp","https://66.media.tumblr.com/d78eca87f8777c91aba8e36bfaf60841/tumblr_piexz3XwSj1ufzrtb_540.jpg")
 
 });
 client.on('error', console.error);
