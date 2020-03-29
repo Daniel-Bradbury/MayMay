0ec939dc9e4971fe802e88347b858521cdb7049d
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-03-29 13:40:45 +0100

    Code rewrite

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// !!! PEOPLE FROM HELP COMMAND SCROLL DOWN TO LINE 37 !!!
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
 const { Client, Attachment } = require('discord.js');
 const fs = require('fs');
+const path = require('path');
+var glob = require("glob");
 
 const client = new Client();
 
@@ -11,103 +10,38 @@ const client = new Client();
  * received from Discord
  */
 client.on('ready', () => {
-  console.log('MayMay ready.');
-  // Link to git
-  client.user.setActivity('m!help', { type: 'WATCHING' });
+	console.log('MayMay ready.');
+	// Link to git
+	client.user.setActivity('m!help', { type: 'WATCHING' });
 });
 
 
 client.on('message', message => {
-function defmeme(id,extension) {
-	if (message.content === ("m!"+id)) {
-		message.delete(1000);
-		console.log(message.author.username+" used "+id)
-		const attachment = new Attachment("./data/"+id+"."+extension);
-		message.reply(attachment);
+	function meme(image) {
+		if (message.content === ("m!" + image.split('.').slice(0, -1).join('.'))) {
+			message.delete(1000);
+			console.log(message.author.username + " used " + image)
+			const attachment = new Attachment("./data/" + image);
+			message.reply(attachment);
+		}
 	}
-}
 
-if (message.content === ("m!help")) {
-	message.reply("https://github.com/Daniel-Bradbury/MayMay/blob/master/index.js");
-}
-if (message.content === ("m!git")) {
-	message.reply("https://github.com/Daniel-Bradbury/MayMay");
-}
+	if (message.content === ("m!help")) {
+		message.reply("https://github.com/Daniel-Bradbury/MayMay/tree/master/data");
+	}
+	if (message.content === ("m!git")) {
+		message.reply("https://github.com/Daniel-Bradbury/MayMay");
+	}
 
-// MEME LIBRARY
-// PREFIX: "m!"
-
-defmeme("politecat","jpg")
-defmeme("lenny","webp")
-defmeme("doge","jpg")
-defmeme("bork","jpg")
-defmeme("groovin","jpg")
-defmeme("bruhmentum","jpg")
-defmeme("lphil","jpg")
-defmeme("longdoge","jpg")
-defmeme("snekcat","jpg")
-defmeme("pepe","jpg")
-defmeme("derp","jpg")
-defmeme("shookderp","gif")
-defmeme("starkderp","gif")
-defmeme("crycat","jpg")
-defmeme("tmyk","jpg")
-defmeme("wat","jpg")
-defmeme("challengereligion","jpg")
-defmeme("takemymoney","jpg")
-defmeme("jojolick","gif")
-defmeme("jojopunch","gif")
-defmeme("dekuheadbang","gif")
-defmeme("blink","gif")
-defmeme("fbiwantslocation","png")
-defmeme("bekfast","jpg")
-defmeme("derpcat","jpg")
-defmeme("areyousure","gif")
-defmeme("hahayes","png")
-defmeme("defdance","gif")
-defmeme("wry","gif")
-defmeme("pepetriggered","jpg")
-defmeme("ohyou","jpg")
-defmeme("nuke","gif")
-defmeme("spideronion","jpg")
-defmeme("sanslmao","png")
-defmeme("unoreverse","png")
-defmeme("derptrump","png")
-defmeme("feelsraveman","gif")
-defmeme("villagertriggered","gif")
-defmeme("omegalul","webp")
-defmeme("endmeemoji","png")
-defmeme("hmm","jpg")
-defmeme("peloangry","webp")
-defmeme("peloangry2","webp")
-defmeme("pelocringe","webp")
-defmeme("pelocrushed","webp")
-defmeme("pelocursed","webp")
-defmeme("pelocute","webp")
-defmeme("pelodance","webp")
-defmeme("pelogrumpy","webp")
-defmeme("pelohappy","webp")
-defmeme("pelohug","webp")
-defmeme("pelomiddlefinger","webp")
-defmeme("pelopeace","webp")
-defmeme("peloscream","webp")
-defmeme("pelosinister","webp")
-defmeme("pelosmile","webp")
-defmeme("pelowut","webp")
-defmeme("shocked","png")
-defmeme("beegyoshi","gif")
-defmeme("godgone","jpg")
-defmeme("watching","jpg")
-defmeme("wholemilk","jpg")
-defmeme("kermitsuicide","gif")
-defmeme("spongedance","gif")
-defmeme("goodboi","png")
-defmeme("fuming","jpg")
-
+	glob("data/*.*", function (er, files) {
+		files.forEach(file => {
+			meme(path.basename(file));
+		});
+	});
 });
 client.on('error', console.error);
 var token;
-token=fs.readFileSync("token.txt");
+token = fs.readFileSync("token.txt");
 token = token.toString().trim();
-console.log("Successfully read token: "+token);
+console.log("Successfully read token: " + token);
 client.login(token);
